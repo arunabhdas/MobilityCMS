6b6d3a43d6823922876357c27373b81d479c34c0
Arunabh Das <arunabhdas@gmail.com>
2015-12-15 06:22:01 -0500

    Added test for edit

--- spec/features/categories/edit_spec.rb
+++ spec/features/categories/edit_spec.rb
@@ -1,12 +1,23 @@
 require 'spec_helper'
 
-describe "Creating categories" do
+describe "Editing categories" do
 
     it "updates a category successfully with correct information" do
         category = Category.create(title: "Science", description: "Science is great for scientists")
         visit "/categories"
-        
+        within "category_#{category.id}" do
+            click_link "Edit"
+        end
 
+        fill_in "Title", with: "My Title"
+        fill_in "Description", with: "My Description"
+        click_button "Update Category"
+
+        category.reload
+
+        expect(page).to have_content("Category was successfully updated")
+        expect(category.title).to eq("My Title")
+        expect(category.description).to eq("My Description")
     end
 
 end
