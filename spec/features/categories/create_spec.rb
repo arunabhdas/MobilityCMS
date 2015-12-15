0782d73bde1c205a0cb5cba703b11fc69a89bffc
Arunabh Das <arunabhdas@gmail.com>
2015-12-15 01:38:40 -0500

    Updated create_spec

--- spec/features/categories/create_spec.rb
+++ spec/features/categories/create_spec.rb
@@ -4,5 +4,6 @@ describe "Creating categories" do
     it "redirects to the categories index page on success" do
         visit "/"
         click_link "New Category"
+        expect(page).to have_content("New Category")
     end
 end
