7614eb868c89dceb7a34ad4949929c199073d9e7
Arunabh Das <arunabhdas@gmail.com>
2015-12-15 04:40:16 -0500

    Updated README

--- spec/features/categories/edit_spec.rb
+++ spec/features/categories/edit_spec.rb
@@ -3,7 +3,9 @@ require 'spec_helper'
 describe "Creating categories" do
 
     it "updates a category successfully with correct information" do
+        category = Category.create(title: "Science", description: "Science is great for scientists")
         visit "/categories"
+        
 
     end
 
