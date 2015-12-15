15e49997ed90e3528fb03d5b1eaa3eb5759bb63b
Arunabh Das <arunabhdas@gmail.com>
2015-12-15 04:12:48 -0500

    Fixed spec to re-use 1 function

--- spec/features/categories/create_spec.rb
+++ spec/features/categories/create_spec.rb
@@ -1,84 +1,64 @@
 require 'spec_helper'
 
 describe "Creating categories" do
+    #---------------------------------------------------------------------------
+    def create_category(options={})
+        options[:title] ||= "My Cateogry Title"
+        options[:description] ||= "My Category Description"
+
+        visit "/"
+        click_link "New Category"
+        expect(page).to have_content("New Category")
+
+        fill_in "Title", with: options[:title]
+        fill_in "Description", with: options[:description]
+        click_button "Create Category"
+    end
+    #---------------------------------------------------------------------------
     it "redirects to the categories index page on success" do
-        visit "/"
-        click_link "New Category"
-        expect(page).to have_content("New Category")
-
-        fill_in "Title", with: "TestCategoryTitle"
-        fill_in "Description", with: "TestDescriptionTitle"
-        click_button "Create Category"
-        expect(page).to have_content("TestCategoryTitle")
+        create_category
+        expect(page).to have_content("Title")
     end
-
+    #---------------------------------------------------------------------------
     it "displays an error when the category has no title" do
-        expect(Category.count).to eq(0)
-
-        visit "/"
-        click_link "New Category"
-        expect(page).to have_content("New Category")
-
-        fill_in "Title", with: ""
-        fill_in "Description", with: "Empty"
-        click_button "Create Category"
-
+        # expect(Category.count).to eq(0)
+        create_category title: ""
         expect(page).to have_content("error")
-        expect(Category.count).to eq(0)
-
+        # expect(Category.count).to eq(0)
         visit "/"
-        expect(page).to_not have_content("Empty")
+        expect(page).to_not have_content("MyDescription")
     end
 
+    #---------------------------------------------------------------------------
     it "displays an error when the category has title less than 3 characters" do
-        expect(Category.count).to eq(0)
+        # expect(Category.count).to eq(0)
 
-        visit "/"
-        click_link "New Category"
-        expect(page).to have_content("New Category")
-
-        fill_in "Title", with: ""
-        fill_in "Description", with: "Empty"
-        click_button "Create Category"
-
+        create_category title: "My"
         expect(page).to have_content("error")
-        expect(Category.count).to eq(0)
+        # expect(Category.count).to eq(0)
 
         visit "/"
         expect(page).to_not have_content("Empty")
     end
-
+    #---------------------------------------------------------------------------
     it "displays an error when the category has no description" do
-        expect(Category.count).to eq(0)
-
-        visit "/"
-        click_link "New Category"
-        expect(page).to have_content("New Category")
-
-        fill_in "Title", with: "MyTitle"
-        fill_in "Description", with: ""
-        click_button "Create Category"
+        # expect(Category.count).to eq(0)
+        create_category title: "Science", description: ""
 
         expect(page).to have_content("error")
-        expect(Category.count).to eq(0)
+        # expect(Category.count).to eq(0)
 
         visit "/"
         expect(page).to_not have_content("MyTitle")
     end
-
+    #---------------------------------------------------------------------------
     it "displays an error when the category has description less than 5 characters" do
-        expect(Category.count).to eq(0)
+        # expect(Category.count).to eq(0)
 
-        visit "/"
-        click_link "New Category"
-        expect(page).to have_content("New Category")
-
-        fill_in "Title", with: "MyTitle"
-        fill_in "Description", with: "MyDe"
-        click_button "Create Category"
+        create_category title: "Science", description: "MyDe"
 
         expect(page).to have_content("error")
-        expect(Category.count).to eq(0)
+        # expect(Category.count).to eq(0)
 
         visit "/"
         expect(page).to_not have_content("Empty")
