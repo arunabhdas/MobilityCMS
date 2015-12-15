ca464334469c60cb69398b9c0ff22d4b07f0406d
Arunabh Das <arunabhdas@gmail.com>
2015-12-15 02:20:37 -0500

    Added test for description length validation

--- spec/features/categories/create_spec.rb
+++ spec/features/categories/create_spec.rb
@@ -12,7 +12,7 @@ describe "Creating categories" do
         expect(page).to have_content("TestCategoryTitle")
     end
 
-    it "displays an error when the todo list has no title" do
+    it "displays an error when the category has no title" do
         expect(Category.count).to eq(0)
 
         visit "/"
@@ -30,7 +30,7 @@ describe "Creating categories" do
         expect(page).to_not have_content("Empty")
     end
 
-    it "displays an error when the todo list has title less than 3 characters" do
+    it "displays an error when the category has title less than 3 characters" do
         expect(Category.count).to eq(0)
 
         visit "/"
@@ -48,7 +48,7 @@ describe "Creating categories" do
         expect(page).to_not have_content("Empty")
     end
 
-    it "displays an error when the todo list has no description" do
+    it "displays an error when the category has no description" do
         expect(Category.count).to eq(0)
 
         visit "/"
@@ -64,5 +64,23 @@ describe "Creating categories" do
 
         visit "/"
         expect(page).to_not have_content("MyTitle")
+    end
+
+    it "displays an error when the category has description less than 5 characters" do
+        expect(Category.count).to eq(0)
+
+        visit "/"
+        click_link "New Category"
+        expect(page).to have_content("New Category")
+
+        fill_in "Title", with: "MyTitle"
+        fill_in "Description", with: "MyDe"
+        click_button "Create Category"
+
+        expect(page).to have_content("error")
+        expect(Category.count).to eq(0)
+
+        visit "/"
+        expect(page).to_not have_content("Empty")
     end
 end
