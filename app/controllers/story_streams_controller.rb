079fdefcd55eb83f54802b5605564a216c0251f6
Arunabh Das <arunabhdas@gmail.com>
2015-12-16 02:11:31 -0500

    Attempting to debug the create_spec test failure

--- app/controllers/story_streams_controller.rb
+++ app/controllers/story_streams_controller.rb
@@ -2,4 +2,27 @@ class StoryStreamsController < ApplicationController
   def index
       @category = Category.find(params[:category_id])
   end
+
+  def new
+      @category = Category.find(params[:category_id])
+      @story_stream = @category.story_streams.new
+  end
+
+  def create
+      @category = Category.find(params[:category_id])
+      @story_stream = @category.story_streams.new(story_stream_params)
+      if @story_stream.save
+          flash[:success] = "Added story_stream"
+          redirect_to category_story_streams_path
+      else
+         flash[:error] = "There was a problem adding that story stream"
+         render action: :new
+    end
+
+  end
+
+  private
+  def story_stream_params
+      params[:story_stream].permit(:content)
+  end
 end
--- spec/features/story_streams/create_spec.rb
+++ spec/features/story_streams/create_spec.rb
@@ -10,14 +10,14 @@ describe "Viewing todo items" do
         end
     end
     #---------------------------------------------------------------------------
-    it "is successfull with valid content" do
+    it "is successful with valid content" do
         visit_category(category)
         click_link "New StoryStream"
         fill_in "Content", with: "Interest Rate Hike"
         click_button "Save"
-        expect(page).to have_content("Added StoryStream")
+        expect(page).to have_content("Added story_stream")
         within("ul.story_streams") do
-            expect(page).to have_content("Added StoryStream")
+            expect(page).to have_content("Added story_stream")
             within("ul.story_streams") do
                 expect(page).to have_content("Interest Rate Hike")
             end
